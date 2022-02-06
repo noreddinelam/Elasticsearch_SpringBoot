b6f9c5f1d9fa1871b5b4bb14f9c30598f2a68102
Nouredine LAMAMRA <noreddine.lamamra@zsoft-consulting.com>
2022-02-06 12:49:18 +0100

    index data and get all Students which have more than 18 years old

--- src/test/java/com/example/elasticsearch/ElasticServiceTest.java
+++ src/test/java/com/example/elasticsearch/ElasticServiceTest.java
@@ -11,8 +11,6 @@ import java.util.List;
 
 @SpringBootTest
 class ElasticServiceTest {
-//    @Autowired
-//    private ElasticRepo elasticRepo;
 
     @Autowired
     private ElasticService elasticService;
@@ -24,7 +22,16 @@ class ElasticServiceTest {
                 "94000 CRETEIL").build());
         listStudents.add(StudentModel.builder().firstName("thibo").lastName("kerdoiyuf").age(17).address("2 BD PABLO PICASSO, " +
                 "94000 CRETEIL").build());
+        listStudents.add(StudentModel.builder().firstName("rashid").lastName("aliev").age(25).address("4 BD PABLO PICASSO, " +
+                "94000 CRETEIL").build());
+        listStudents.add(StudentModel.builder().firstName("abdallah").lastName("idris").age(98).address("10 BD PABLO PICASSO, " +
+                "94000 CRETEIL").build());
+        listStudents.add(StudentModel.builder().firstName("dasilva").lastName("junior").age(13).address("17 BD PABLO PICASSO, " +
+                "94000 CRETEIL").build());
+        listStudents.add(StudentModel.builder().firstName("idk").lastName("idnk").age(25).address("17 BD PABLO PICASSO, " +
+                "94000 CRETEIL").build());
         List<StudentModel> list = elasticService.indexStudents(listStudents);
+        assertThat(list).isEqualTo(listStudents);
     }
 
     @Test
